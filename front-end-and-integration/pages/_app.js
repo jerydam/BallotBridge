aed08124f78fa1143ad9b4b0830a24e4ff81be48
mrpatrick030 <patrickmaebi@gmail.com>
2024-09-04 08:31:09 +0100

    front end and integration

--- front-end-and-integration/pages/_app.js
+++ front-end-and-integration/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Web3Modal } from '@/context/web3modal';
+export const metadata = {
+  title: 'BallotBridge',
+  description: 'The leading platform for governmental elections in Africa.'
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  (
+         <>
+           <Web3Modal>
+           <Component {...pageProps} />
+           </Web3Modal>
+          </>
+          )
 }
